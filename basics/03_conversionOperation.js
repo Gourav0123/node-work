4f5e5d48f312ecde6020bc47f8de8044cbe6e011
dharamconjoniix <ashish.sharma@conjoinix.com>
2026-03-11 16:59:07 +0530

    conversion code added

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+// let score = "33abc";
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = "Gourav";
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+//********************************Operation******************************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+let str1 = "hello";
+let str2 = "Gourav";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(("1" + 2 +2));
+console.log((1 + 2 + "2"));  // 32 ~ Faaaaaaaaaah , what ?
+
+console.log((3 + 4) * 5 % 3);
+console.log(4+5);
+console.log("Added more lines to the code");
+
+
