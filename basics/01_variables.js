608de4f33384e59c487886bb2b7db2904be0e6a9
dharamconjoniix <ashish.sharma@conjoinix.com>
2026-03-11 16:20:36 +0530

    added two more files - data types and variables

---
+++ basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId = 144553;
+let accountEmail = "gourav@gmail.com";
+accountPassword = "@1234"
+accountCity = "Chandigarh"
+console.table([accountEmail, accountId, accountCity, accountPassword]);
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+
+console.log("Gourav");
+
+let name = "Gourav";
+let age = 23;
+let isLoggedIn = false;
+let state;
+
+//number => 2 to power  53
+//bigint
+//string => ""
+//Boolean => true/false
+//null => standalone Value
+//undefined =>
+//symbol => unique
+
+//object 
+
+console.log(typeof "Gourav");
+console.log(typeof null);   //object
+
